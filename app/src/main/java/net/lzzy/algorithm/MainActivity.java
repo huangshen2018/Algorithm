89ab1851257416566f43491ca37b0745d0c0dcce
17377203145 <94676033@qq.com>
2019-06-08 16:29:56 +0800

    直接选择排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -74,12 +74,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int num=10;
         for (int i=0;i<num-1;i++){
             for (int j=0;j<num-1;j++){
-                if (items[j]>items[j+1])
-                    {
+                if (items[j]>items[j+1]) {
                     int temp;
-                    temp=items[j];
-                    items[j]=items[j+1];
-                    items[j+1]=temp;
+                    temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
                 }
             }
         }
